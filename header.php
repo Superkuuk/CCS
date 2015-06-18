b56315d896d656c95d8a0dc68d53abafc5df9aff
jelleklaver <info@jelleklaver.com>
2015-06-18 12:12:50 +0200

    Werkende slideshow

--- header.php
+++ header.php
@@ -1,9 +1,6 @@
 <div id="slideshow">
     
-    <img src="images/header01.jpg" class="active img__01__">
-    <img src="images/header02.jpg" class="img__02__">
-    <img src="images/header01.jpg" class="img__03__">
-    <img src="images/header02.jpg" class="img__04__">
+    <img src="images/header.jpg">
 
 </div>
 
@@ -12,10 +9,10 @@
 </div>
 
 <div id="balls">
-    <img src="images/ball-fill.png" class="active img__01__">
+    <img src="images/ball-fill.png" class="active img__00__">
+    <img src="images/ball-open.png" class="img__01__">
     <img src="images/ball-open.png" class="img__02__">
-    <img src="images/ball-open.png" class="img__03__">
-    <img src="images/ball-open.png" class="img__04__">    
+    <img src="images/ball-open.png" class="img__03__">    
 </div>
 
 
@@ -41,46 +38,30 @@ $(window).load( function() {
     
     var $nextball;
     var sliding = false;
+    var olddiff = 0; var oldpos = 0; var pos;
+    
     //Automatically loop images and 'balls'
     function slideSwitch($nextball) {
         
-        //Select active image and active ball.
-        var $active = $('#slideshow img.active');
+        //Select active ball.
         var $activeball = $('#balls img.active');
             
-            //In case no $nextball was inserted in the function, it will automatically select the next image (and thus next ball).
-            if(!$nextball) {
+        //In case no $nextball was inserted in the function, it will automatically select the next image (and thus next ball).
+        if(!$nextball) {
                 $nextball =  $activeball.next().length ? $activeball.next() : $('#balls img:first');    
-            }
-                console.log($nextball.hasClass('active'));
+        }
+                
+        var tempnext = $nextball.attr('class').split('__');
+        var nextnumber = tempnext[1];
+       
+        //Move image.
+        $('#slideshow img').dequeue()
+                        .animate({left: - nextnumber * $(this).width()}, 1000);
 
-            //If selected ball is already active.
-            if($nextball.hasClass('last-active')) {
-                console.log('Actief');
-            }
-
-            //Get number of selected ball and previous ball.
-            var tempactive = $activeball.attr('class').split('__');
-            var activenumber = tempactive[1];
-            var tempnext = $nextball.attr('class').split('__');
-            var nextnumber = tempnext[1];
-        
-            //Make sure next image corresponds to selected ball
-            var $next = $('#slideshow img.img__'+nextnumber+'__');
-
-            //Check if new image is more to right than old image.
-                var diff = parseInt(nextnumber) - parseInt(activenumber);
-                $('#slideshow img').each( function() {
-                    $(this).dequeue()
-                    .animate({left: Math.floor($(this).position().left / $(document).width()) * $(document).width() - diff * $(this).width() }, 1000);
-                });    
-
-
-            //Change image of ball
-            $activeball.attr("src", "images/ball-open.png").removeClass('active last-active');
-            $nextball.attr("src", "images/ball-fill.png").addClass('active');
+        //Change image of ball
+        $activeball.attr("src", "images/ball-open.png").removeClass('active last-active');
+        $nextball.attr("src", "images/ball-fill.png").addClass('active');
         $nextball = null;
-      //  }
     }
     
 </script>
