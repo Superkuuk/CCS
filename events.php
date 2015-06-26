21f81852b53fc18d814ffa7d0487ab425df715ab
jelleklaver <info@jelleklaver.com>
2015-06-26 19:50:47 +0200

    Events v0.001 + htaccess
    .htaccess file zorgt voor een redirect naar CSS/index.php voor alle NIET
    bestaande filepaths. LET WEL, hij redirect nu naar CSS/index.php, moet
    veranderd bij de website

--- events.php
+++ events.php
@@ -1,38 +1,90 @@
-Eventspage.<br><br>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nam rhoncus eros at augue posuere laoreet. Nam ut risus a ex pulvinar pharetra. 
-            Praesent sollicitudin odio eget urna faucibus, eget fringilla leo tincidunt. Fusce 
-            quis porttitor est. Etiam non nisi eu ligula malesuada venenatis ac non quam.
-             Suspendisse malesuada leo vestibulum dolor vulputate tempus. Duis quam odio, 
-             efficitur ac rhoncus ac, posuere ac nisi. Mauris sit amet tempor justo. Vivamus
-              porttitor faucibus ligula at imperdiet. Ut at pulvinar nibh, at porttitor nulla.
-               Aenean tellus nisl, ullamcorper sit amet erat ut, volutpat malesuada nisi. 
-               Maecenas mi elit, vestibulum sed odio sit amet, gravida hendrerit justo. 
-               Vestibulum suscipit luctus metus, nec interdum mauris euismod et. Vivamus 
-               imperdiet sit amet nisl sit amet volutpat. Nunc in ultricies neque. Vestibulum 
-               arcu purus, interdum eu elementum sed, tincidunt sit amet turpis. Donec eget 
-               risus a augue maximus volutpat id sed mi. Vestibulum in magna ac nulla malesuada 
-               varius et at tortor. Sed hendrerit diam sit amet cursus venenatis. Aenean tincidunt
-                semper tempor. In neque ipsum, ultricies convallis sem ac, gravida mollis
-                 felis. Proin vel pellentesque risus, nec tincidunt ligula. Ut et pellentesque 
-                 odio. Morbi pharetra diam et ex dignissim, a consectetur purus efficitur.
-                  Donec sollicitudin neque quis hendrerit scelerisque. Proin a lacus in purus 
-                  gravida ullamcorper id eget odio.
-    	            Nam rhoncus eros at augue posuere laoreet. Nam ut risus a ex pulvinar pharetra. 
-            Praesent sollicitudin odio eget urna faucibus, eget fringilla leo tincidunt. Fusce 
-            quis porttitor est. Etiam non nisi eu ligula malesuada venenatis ac non quam.
-             Suspendisse malesuada leo vestibulum dolor vulputate tempus. Duis quam odio, 
-             efficitur ac rhoncus ac, posuere ac nisi. Mauris sit amet tempor justo. Vivamus
-              porttitor faucibus ligula at imperdiet. Ut at pulvinar nibh, at porttitor nulla.
-               Aenean tellus nisl, ullamcorper sit amet erat ut, volutpat malesuada nisi. 
-               Maecenas mi elit, vestibulum sed odio sit amet, gravida hendrerit justo. 
-               Vestibulum suscipit luctus metus, nec interdum mauris euismod et. Vivamus 
-               imperdiet sit amet nisl sit amet volutpat. Nunc in ultricies neque. Vestibulum 
-               arcu purus, interdum eu elementum sed, tincidunt sit amet turpis. Donec eget 
-               risus a augue maximus volutpat id sed mi. Vestibulum in magna ac nulla malesuada 
-               varius et at tortor. Sed hendrerit diam sit amet cursus venenatis. Aenean tincidunt
-                semper tempor. In neque ipsum, ultricies convallis sem ac, gravida mollis
-                 felis. Proin vel pellentesque risus, nec tincidunt ligula. Ut et pellentesque 
-                 odio. Morbi pharetra diam et ex dignissim, a consectetur purus efficitur.
-                  Donec sollicitudin neque quis hendrerit scelerisque. Proin a lacus in purus 
-                  gravida ullamcorper id eget odio.
+<div id="event-read-more-block">
+    <div id="event-left" style="overflow-y: hidden">
+        <p class="event-row-01">
+            Deze informatie  meer info meer info meer info is nog veel leuker. 
+            Veel plezier met het maken van de website Rutger en Jelle. 
+            Voor vragen weten jullie ons te vinden!
+        </p>            
+        <p class="event-row-02">
+            Hier komt de extra informatie wanneer je op het plusje drukt met een leuke extra fotoooo. 
+            Normaal gesproken zou de er onder de afbeelding eenzelfde ruimte zijn als de rode streepjes aangeven. 
+            Maar deze pagina heeft een beperkte grootte
+        </p>
+    </div>
+    <div id="event-right">
+        <img id="event-img-01" class="event-row-01" src="images/product_01.jpg" alt="Hier komt een afbeelding van een festival.">
+        <img id="event-img-02" class="event-row-02" src="images/product_01.jpg" alt="Hier komt een afbeelding van een festival.">
+    </div>
+</div>
+
+	<script>
+
+            // Zelf in te vullen:
+            var hoogte_block;	// wordt nog afgerond naar het meest naarbije getal wat goed uitkomt met line-height
+            var read_more_text_open = "+";
+            var read_more_text_closed = "-";
+        
+            var real_height;
+            var expanded = false;
+            console.log($('#event-left').height());
+            $('#event-img01, #event-img02').load( function() {
+            //Set height of rows equal
+            $('p.event-row-01').height($('img.event-row-01').height());
+            $('p.event-row-02').height($('img.event-row-02').height());
+            $('.event-row-02').css('margin-top', $('#mainnav').outerHeight());
+            $('#event-read-more-block').height($('#event-left').height() * 1.35);
+            $('#event-read-more-block').width($('#event-left').outerWidth(true) + $('#event-right').outerWidth(true));	
+            
+
+            // automated calculated terminated
+            var fontSize = $("#event-read-more-block").css('font-size');
+            var lineHeight = Math.floor(parseInt(fontSize.replace('px','')) * 1.5);
+            var expanded = false;
+            var hoogte_block = $('#event-img01').height();
+
+            hoogte_block = Math.floor(hoogte_block / lineHeight) * lineHeight;
+            console.log($('#event-read-more-block').height());
+            if($("#event-read-more-block").height() > hoogte_block){
+                $("#event-left, #event-right").css({
+                    'height': hoogte_block,
+                    'overflow': 'hidden'
+                });
+
+                // Pas span aan naar gewenste opmaak
+                $("#event-left .event-row-01").append('<span id="event-expand" style="cursor: pointer; color: rgba(109,207,246,1)" onclick="readMore();">'+read_more_text_open+'</span>');
+            }
+        });
+        
+
+    	
+        function readMore(){
+            if(expanded == false){
+                var real_height = $("#event-read-more-block").height();
+                $("#event-left, #event-right").animate({
+                    'height': real_height+'px'
+                }, 500, function(){
+                    // animation done
+                    $("#event-left .event-row-02").append('<span id="event-expand" style="cursor: pointer; color: rgba(109,207,246,1)" onclick="readMore();"></span>');
+                    $("#event-expand").html(read_more_text_closed);
+                    expanded = true;
+                });
+                $('body, html').animate({scrollTop: ($('#event-read-more-block').offset().top + $('#event-read-more-block').outerHeight(true)) - $(window).outerHeight(true) + 75}, 500);
+                $('#event-expand').remove();
+            }else{
+                hoogte_block = $('#event-img01').height();
+                $("#event-left, #event-right").animate({
+                    'height': hoogte_block+'px'
+                }, 500, function(){
+                    // animation done
+                    $('#event-expand').remove();
+                    $("#event-left .event-row-01").append('<span id="event-expand" style="cursor: pointer; color: rgba(109,207,246,1)" onclick="readMore();"></span>');
+                    $("#event-expand").html(read_more_text_open);
+                    expanded = false;
+                });
+                $('body, html').animate({scrollTop: 0}, 500);
+            }
+        }
+        
+
+
+	</script>
